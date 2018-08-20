8963cb88dde271afc937101d658609c0a9620a6d
PlayMa256 <matheus.g.silva@hotmail.com>
2018-08-19 23:01:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+ import { cube } from './index3.js';
+
+  function component() {
+   var element = document.createElement('div');
+   var element = document.createElement('pre');
+
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.innerHTML = [
+     'Hello webpack!',
+     '5 cubed is equal to ' + cube(5)
+   ].join('\n\n');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
